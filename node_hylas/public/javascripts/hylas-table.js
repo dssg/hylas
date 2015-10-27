fd1d3c02e28bcd06a9892f3158bad1d162d9fa1d
zar1 <zachary.rubenstein@gmail.com>
2015-10-27 15:25:48 -0500

    pulls best features from server

--- node_hylas/public/javascripts/hylas-table.js
+++ node_hylas/public/javascripts/hylas-table.js
@@ -1,5 +1,13 @@
+//http://stackoverflow.com/questions/8749236/create-table-with-jquery-append
 $(document).ready(function() {
     $.getJSON('/data/top_features', function(jd) {
-        $('#table').text(jd.data);
+        var table = $('<table></table>');
+        var row = $('<tr></tr>');
+        jd.data.forEach( function(th) {
+            var cell = $('<th></th>').text(th);
+            row.append(cell);
+        });
+        table.append(row);
+        $('#hylas_table').append(table);
     });
 });
