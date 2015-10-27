be4142cc16593edfd2f18fe0a9a41231f4ca3082
zar1 <zachary.rubenstein@gmail.com>
2015-10-27 11:24:00 -0500

    successfully forwarding requests to Python!

--- node_hylas/routes/index.js
+++ node_hylas/routes/index.js
@@ -1,18 +1,26 @@
 var express = require('express');
 var http = require('http');
+var url = require('url');
 
 var router = express.Router();
 
-router.get('/top_features', function(req, res, next) {
+router.get('/data/*', function(req, res, next) {
     var callback = function(response) {
         var body = '';
         response.on('data', function(data) {
             body += data;
         });
             response.on('end', function() {
-                //TODO render something
+                res.send(body);
         });
     }
+    var dataUrl = req.url.substring(req.url.lastIndexOf('/'));
+    // http://stackoverflow.com/questions/6912584/how-to-get-get-query-string-variables-in-express-js-on-node-js
+    var data_req = http.get({
+        host: 'localhost',
+        port: '5000',
+        path: dataUrl
+    }, callback);
 });
 
 /* GET home page. */
