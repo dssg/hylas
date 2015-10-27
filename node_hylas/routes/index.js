ad47a6de8e2c737c6c55dc545c800d74a8463af2
zar1 <zachary.rubenstein@gmail.com>
2015-10-27 09:59:55 -0500

    changed 2 spaces to 4 spaces

--- node_hylas/routes/index.js
+++ node_hylas/routes/index.js
@@ -1,9 +1,23 @@
 var express = require('express');
+var http = require('http');
+
 var router = express.Router();
+
+router.get('/top_features', function(req, res, next) {
+    var callback = function(response) {
+        var body = '';
+        response.on('data', function(data) {
+            body += data;
+        });
+            response.on('end', function() {
+                //TODO render something
+        });
+    }
+});
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+    res.render('index', { title: 'Express' });
 });
 
 module.exports = router;
