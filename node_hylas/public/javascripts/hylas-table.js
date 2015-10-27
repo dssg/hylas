1133e46a9c7c390a53fc2ccc04934696171cd537
zar1 <zachary.rubenstein@gmail.com>
2015-10-27 15:54:20 -0500

    client pulls down and builds table

--- node_hylas/public/javascripts/hylas-table.js
+++ node_hylas/public/javascripts/hylas-table.js
@@ -1,13 +1,30 @@
 //http://stackoverflow.com/questions/8749236/create-table-with-jquery-append
 $(document).ready(function() {
     $.getJSON('/data/top_features', function(jd) {
+        var topFeatures = jd.data;
         var table = $('<table></table>');
         var row = $('<tr></tr>');
-        jd.data.forEach( function(th) {
+        topFeatures.forEach(function(th) {
             var cell = $('<th></th>').text(th);
             row.append(cell);
         });
         table.append(row);
-        $('#hylas_table').append(table);
+        console.log('?cols=' + topFeatures.toString());
+        $.getJSON('/data/top_units?n=10&cols=' + topFeatures.toString(), 
+            function(jd) {
+            var columns = jd.data;
+            var nRows = columns[Object.keys(columns)[0]].length;
+            for (var i = 0; i < nRows; i++) {
+                var row = $('<tr></tr>');
+                topFeatures.forEach(function(colName) {
+                    var td = columns[colName][i];
+                    var cell = $('<td></td>').text(td.toString().slice(0, 4));
+                    row.append(cell);
+                });
+                table.append(row)
+            }
+            $('#hylas_table').append(table);
+        });
     });
+
 });
