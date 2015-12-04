12af44f57bec5d80fd824d545aadc415c9492f9d
zar1 <zachary.rubenstein@gmail.com>
2015-12-04 15:19:34 -0600

    making a bunch of changes at once, which is dump

--- univis/public/javascripts/controller.js
+++ univis/public/javascripts/controller.js
@@ -3,7 +3,42 @@
         .controller('univisCtrl', ['$scope', 'dataservice', 'uniplot',
             function($scope, dataservice, uniplot) {
 
-        var updateModelInfo = function() {
+        // initialization
+        $scope.model_id = undefined;
+        $scope.unit_id = undefined;
+        $scope.open_view = undefined;
+        $scope.selected_feature = undefined;
+        $scope.model_list = [];
+        $scope.model_info = {};
+        $scope.roc = {};
+        $scope.pr = {};
+        $scope.top_features = [];
+        $scope.top_n_feature_names = [];
+        $scope.top_units = [];
+        $scope.top_unit_features = [];
+        $scope.unit = {};
+        $scope.selected_feature = 'Choose a Feature';
+        $scope.similar_units = [];
+        $scope.similar_unit_features = [];
+        $scope.dist = {};
+        $scope.model_picked = false;
+        $scope.unit_picked = false;
+
+        $scope.$watch('model_id', updateModel);
+        $scope.$watch('unit_id', updateUnit);
+        $scope.$watch('open_view', updateView);
+        $scope.$watch('selected_feature', updateFeature);
+
+        $scope.$watch('model_info', updateModelInfo);
+
+        dataservice.getListModels().then(function (data) {
+            $scope.model_list = data;
+        });
+
+        $scope.open_view = 'models';
+
+        // functions
+        function updateModelInfo(new_value) {
             uniplot.line(
                 $scope.roc,
                 $scope.model_info.graphs.roc.fpr,
@@ -20,9 +55,9 @@
                 'precision');
         }
 
-        var updateSelectedFeature = function () {
-            feature = $scope.selected_feature;
-
+        function updateFeature(new_value) {
+            feature = new_value;
+            console.log('feature = ' + feature);
             dataservice.getSimilar($scope.model_id, $scope.unit_id)
                 .then( function (data) {
                     $scope.similar_units = data;
@@ -45,73 +80,7 @@
                 });
         }
 
-
-        $scope.pickModel = function ($index) {
-            $scope.model_id = $index;
-            $scope.open_view='model_performance';
-        }
-
-        $scope.pickUnit = function ($index, pool) {
-            if (pool === 'similar') {
-                $scope.unit_id = $scope.similar_units[$index].unit_id;
-                $scope.pickFeature($scope.selected_feature);
-            } else {
-                $scope.unit_id = $scope.top_units[$index].unit_id;
-            }
-            console.log('unit id= ' + $scope.unit_id);
-            var feature_list = $scope.top_n_feature_names.join(',');
-            dataservice.getUnit(
-                    $scope.model_id, 
-                    $scope.unit_id, 
-                    $scope.top_n_feature_names)
-                .then(function (data) {
-                    $scope.unit = data;
-                });
-            //TODO for multiple features
-            updateSelectedFeature();    
-            $scope.unit_picked = true;
-            $scope.open_view = 'unit_performance';
-        }
-
-
-        $scope.pickFeature = function (feature) {
-            $scope.selected_feature = feature;
-            console.log('similar feature = ' + $scope.selected_feature);
-            updateSelectedFeature();    
-        }
-
-        $scope.$watch('open_view', function (new_value) {
-            var place = new_value;
-            console.log('going to: ' + place);
-            $scope.view_models_open = false;
-            $scope.view_model_performance_open = false;
-            $scope.view_unit_performance_open = false;
-            if (place === 'model_performance') {
-                $scope.view_model_performance_open = true;
-                return;
-            }
-            if (place === 'unit_performance') {
-                $scope.view_unit_performance_open = true;
-                return;
-            }
-            $scope.view_models_open = true;
-        });
-
-        $scope.goTo = function (place, $event) {
-            if (typeof $event !== 'undefined') {
-                $event.preventDefault();
-                $event.stopPropagation();
-                if (place === 'model_performance' && !$scope.model_picked) {
-                    return;
-                }
-                if (place === 'unit_performance' && !$scope.unit_picked) {
-                    return;
-                }
-            }
-            $scope.open_view = place
-        }
-
-        $scope.$watch('model_id', function (new_value) {
+        var updateModel = function(new_value) {
             if (new_value === undefined) return;
             console.log('model id= ' + $scope.model_id);
             dataservice.getTopUnits($scope.model_id)
@@ -142,29 +111,75 @@
                     updateModelInfo();
                 });
             $scope.model_picked = true;
-        });
+        }
 
-        $scope.model_list = [];
-        $scope.model_info = {};
-        $scope.roc = {};
-        $scope.pr = {};
-        $scope.top_features = [];
-        $scope.top_n_feature_names = [];
-        $scope.top_units = [];
-        $scope.top_unit_features = [];
-        $scope.unit = {};
-        $scope.selected_feature = 'Choose a Feature';
-        $scope.similar_units = [];
-        $scope.similar_unit_features = [];
-        $scope.dist = {};
-        $scope.model_picked = false;
-        $scope.unit_picked = false;
+        var updateUnit = function(new_value) {
+            console.log('unit id= ' + $scope.unit_id);
+            var feature_list = $scope.top_n_feature_names.join(',');
+            dataservice.getUnit(
+                    $scope.model_id, 
+                    $scope.unit_id, 
+                    $scope.top_n_feature_names)
+                .then(function (data) {
+                    $scope.unit = data;
+                });
+            //TODO for multiple features
+            updateSelectedFeature();    
+            $scope.unit_picked = true;
+        }
 
-        dataservice.getListModels().then(function (data) {
-            $scope.model_list = data;
-        });
+        var updateView = function(new_value) {
+            var place = new_value;
+            console.log('going to: ' + place);
+            $scope.view_models_open = false;
+            $scope.view_model_performance_open = false;
+            $scope.view_unit_performance_open = false;
+            if (place === 'model_performance') {
+                $scope.view_model_performance_open = true;
+                return;
+            }
+            if (place === 'unit_performance') {
+                $scope.view_unit_performance_open = true;
+                return;
+            }
+            $scope.view_models_open = true;
+        }
 
-        $scope.open_view = 'models';
+        $scope.pickModel = function ($index) {
+            $scope.model_id = $index;
+            $scope.open_view='model_performance';
+        }
+
+        $scope.pickUnit = function ($index, pool) {
+            if (pool === 'similar') {
+                $scope.unit_id = $scope.similar_units[$index].unit_id;
+            } else {
+                $scope.unit_id = $scope.top_units[$index].unit_id;
+            }
+            $scope.open_view = 'unit_performance';
+        }
+
+
+        $scope.pickFeature = function (feature) {
+            $scope.selected_feature = feature;
+            updateSelectedFeature();    
+        }
+
+
+        $scope.goTo = function (place, $event) {
+            if (typeof $event !== 'undefined') {
+                $event.preventDefault();
+                $event.stopPropagation();
+                if (place === 'model_performance' && !$scope.model_picked) {
+                    return;
+                }
+                if (place === 'unit_performance' && !$scope.unit_picked) {
+                    return;
+                }
+            }
+            $scope.open_view = place
+        }
+
 
     }]);
 })();
