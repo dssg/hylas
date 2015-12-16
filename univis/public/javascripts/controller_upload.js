78f2b527b371af171c7a2cc68233afd00104cfbe
zar1 <zachary.rubenstein@gmail.com>
2015-12-16 16:38:16 -0600

    2nd page works

---
+++ univis/public/javascripts/controller_upload.js
@@ -0,0 +1,8 @@
+(function () {
+    angular.module('univisApp')
+        .controller('uploadCtrl', ['$scope', 'dataservice', 
+            function($scope, dataservice) {
+                $scope.message = 'UPLOAD HERE';
+
+    }]);
+})();
--- univis/public/javascripts/routing.js
+++ univis/public/javascripts/routing.js
@@ -3,9 +3,16 @@
     .config(['$routeProvider',
         function($routeProvider) {
             $routeProvider.
-                when('/', {
+                when('/report', {
                     templateUrl: 'views/report.html',
                     controller: 'reportCtrl'
+                })
+                .when('/upload', {
+                    templateUrl: 'views/upload.html',
+                    controller: 'uploadCtrl'
+                })
+                .otherwise({
+                    redirectTo: '/report'
                 });
         }]);
 })()
