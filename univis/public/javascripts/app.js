cf059ba3bf38cb9a219cae429ed3f0592fbbf8da
zar1 <zachary.rubenstein@gmail.com>
2015-12-17 10:36:42 -0600

    working on that file upload

--- univis/public/javascripts/app.js
+++ univis/public/javascripts/app.js
@@ -1,3 +1,3 @@
 (function () {
-    angular.module('univisApp', ['ngAnimate', 'ngRoute', 'ui.bootstrap', 'nvd3']);
+    angular.module('univisApp', ['ngAnimate', 'ngRoute', 'ui.bootstrap', 'nvd3',                                 'ngFileUpload' ]);
 })();
--- univis/public/javascripts/controller_upload.js
+++ univis/public/javascripts/controller_upload.js
@@ -2,7 +2,6 @@
     angular.module('univisApp')
         .controller('uploadCtrl', ['$scope', 'dataservice', 
             function($scope, dataservice) {
-                $scope.message = 'UPLOAD HERE';
 
     }]);
 })();
